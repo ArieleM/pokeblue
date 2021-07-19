f65130497c7a2bee05c6ffd9bb6871152fed87e6
Ariele Mendonça <ariele_apm@outlook.com>
2021-07-18 23:37:39 -0300

    Add toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from "next/app";
 import "../styles/global.scss";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default MyApp;
