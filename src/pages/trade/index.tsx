f7a375de3907dbfe9ec3de0fda70ce47635840c7
Ariele Mendonça <ariele_apm@outlook.com>
2021-07-18 22:06:28 -0300

    Load data pokeapi

--- src/pages/trade/index.tsx
+++ src/pages/trade/index.tsx
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import styles from "./styles.module.scss";
 import Bag from "../components/Bag";
+import { GetStaticProps } from "next";
+import { pokeapi } from "../../services/api";
 
-export default function Trade() {
+interface IAllPokemon {
+  name: string;
+  url: string;
+}
+
+interface TradeProps {
+  allPokemon: IAllPokemon[];
+}
+
+export default function Trade({ allPokemon }: TradeProps) {
+  console.log(allPokemon);
+
   return (
     <>
       <Head>
@@ -11,10 +24,23 @@ export default function Trade() {
       <main className={styles.container}>
         <h1>Temos que trocar!</h1>
         <div style={{ display: "flex" }}>
-          <Bag />
-          <Bag />
+          <Bag allPokemon={allPokemon} />
+          <button>Troca</button>
+          <Bag allPokemon={allPokemon} />
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await pokeapi.get("/pokemon/?limit=1118");
+  const allPokemon: IAllPokemon[] = await data.results;
+
+  return {
+    props: {
+      allPokemon,
+    },
+    revalidate: 60 * 60 * 24, //24h
+  };
+};
